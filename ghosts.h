da588bb763b42aeeec7cace5d37c76fc905d3dcf
Ryan Costin <143475060+RTCostin@users.noreply.github.com>
2024-03-14 02:48:53 +0000

    Rough outline for ghosts and sprite added

--- ghosts.h
+++ ghosts.h
@@ -1,4 +1,35 @@
 #ifndef GHOSTS_H
 #define GHOSTS_H
 
+// Ghosts
+
+// Display
+// 	3 stages
+// 	-alive
+// 	-blue
+// 	-eyes
+	
+// 	2 separate sprites to appear moving
+// 	-applies to above stages except eyes stage
+
+// 	4 sprite
+
+// Rough draft sprite in "Red_Ghost_Sprite.png"
+
+// Movement
+// 	-Must stay within bounds
+// 	-Cannot go through walls
+// 	-Can move through eachother
+// 	-Alive stage ghosts chase player
+// 	-Blue stage ghosts avoid player
+// 	-Eyes stage ghosts return to "base"
+// 	-(Can add different movement patterns later)
+	
+
+// Actions
+// 	-When ghosts touch player in alive stage, player dies
+// 	-When ghosts touch playre in blue stage, ghosst turn to eyes stage
+// 	-When ghosts touch player in eyes stage, nothing happens
+
+
 #endif
